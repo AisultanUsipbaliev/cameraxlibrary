df85cafbeb60437d7051f45dcb5976d796985821
aisultan <aisultanusipbaliev@gmail.com>
2024-02-22 16:04:42 +0600

    Update

--- app/src/main/java/com/avrora/telecom/exampleuselibrary/MainActivity.kt
+++ app/src/main/java/com/avrora/telecom/exampleuselibrary/MainActivity.kt
@@ -1,14 +1,14 @@
 package com.avrora.telecom.exampleuselibrary
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+//import com.avrora.telecom.cameraxlibrary.MainActivity
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-//        val intent = Intent(this, BorderView::class.java)
+//        val intent = Intent(this, MainActivity::class.java)
 //        startActivity(intent)
 
         finish()
